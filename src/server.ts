5adf47925d3052c383e28ac975088f6001cb7b3c
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-12-22 18:38:23 -0300

    feat: add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import 'dotenv/config';
+import 'express-async-errors';
+
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 3333);
